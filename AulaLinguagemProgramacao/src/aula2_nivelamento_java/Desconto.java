ddc785f1d9230c1a8edd19f4ef6ed7961c3d8213
cesarvecchio <53615643+cesarvecchio@users.noreply.github.com>
2020-02-17 15:01:43 -0300

    Add files via upload

---
+++ AulaLinguagemProgramacao/src/aula2_nivelamento_java/Desconto.java
@@ -0,0 +1,23 @@
+package aula2_nivelamento_java;
+
+import java.util.Scanner;
+
+public class Desconto {
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in);
+		Double valorProduto;
+		Integer codigoDesconto;
+		
+		System.out.println("Digite o valor do produto");
+		valorProduto = leitor.nextDouble();
+		
+		
+		System.out.println("Digite o código de desconto");
+		System.out.println("______________________________");
+		System.out.println("| Código Desconto | Desconto |");
+		System.out.println("______________________________");
+		
+	}
+
+}
